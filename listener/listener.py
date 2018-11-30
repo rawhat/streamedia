1c1143cfd42049f0902abe8369b57db8dd7fce59
Alex Manning <alex41290@gmail.com>
2018-11-30 15:37:31 -0500

    kinda think this won't work... next commit will be .net

--- listener/listener.py
+++ listener/listener.py
@@ -1,34 +1,50 @@
 import socket
 import soundcard as sc
 import sys
+import wave
 
-# UDP_IP_ADDR = "255.255.255.255"
-UDP_IP_ADDR = "192.168.173.191"
-UDP_PORT_NO = 3001
+TCP_IP_ADDR = "192.168.173.191"
+# TCP_IP_ADDR = "127.0.0.1"
+TCP_PORT_NO = 3001
 
 def run():
   print("connecting to socket server")
-  client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  # client.connect((TCP_IP_ADDR, TCP_PORT_NO))
   print("getting all microphones")
-  all_microphones = sc.all_microphones(include_loopback=True)
-  print("got all microphones: {}".format(all_microphones))
-  loopback = next((microphone for microphone in all_microphones if microphone.isloopback and microphone.name.find('Speakers') != -1), None)
+  # all_microphones = sc.all_microphones(include_loopback=True)
+  # print("got all microphones: {}".format(all_microphones))
+  # loopback = next((microphone for microphone in all_microphones if microphone.isloopback and microphone.name.find('Speakers') != -1), None)
+  loopback = sc.default_microphone()
   if loopback is None:
     print("No loopback device detected, exiting...")
     exit(1)
   print("Found a loopback microphone with: {}".format(loopback))
-  with loopback.recorder(samplerate=44100, channels=2) as mic:
-    while True:
-      try:
-        data = mic.record()
-        byte = data.tobytes()
-        print(byte)
-        sys.stdout.flush()
-        # client.sendto(byte, (UDP_IP_ADDR, UDP_PORT_NO))
-        # mic.flush()
-      except Exception as e:
-        print("Uh oh... failed: {}".format(e))
-        exit(1)
+  try:
+    with loopback.recorder(samplerate=44100, channels=2) as mic:#, wave.open('test.wav', 'wb') as wav:
+      # wav.setparams((1, 2, 44100, 0, 'NONE', 'NONE'))
+      spk = next(speaker for speaker in sc.all_speakers() if speaker.name.find('ODAC') != -1)
+      print(spk)
+      # mic.__enter__()
+      # while True:
+      while True:
+        try:
+          data = loopback.record(1024, 44100)
+          spk.play(data, 44100)
+          # byte = data.tobytes()
+          # wav.writeframes(byte)
+          # mic.flush()
+          # byte = bytes('testing', 'utf-8')
+          # client.send(byte)
+          # sys.stdout.flush()
+          # client.sendto(byte, (UDP_IP_ADDR, UDP_PORT_NO))
+          # mic.flush()
+        except Exception as e:
+          print("Uh oh... failed: {}".format(e))
+          exit(1)
+  except KeyboardInterrupt:
+    # client.close()
+    wav.close()
 
 if __name__ == "__main__":
   run()
--- player/player.py
+++ player/player.py
@@ -4,20 +4,20 @@ import soundcard as sc
 
 def play():
   all_speakers = sc.all_speakers()
-  # print(all_speakers)
   output = all_speakers[1]
-  # output = sc.default_speaker()
-  print(output)
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.bind(('', 3001))
+  sock.listen(1)
+  conn, addr = sock.accept()
+
   player = output.player(44100)
   player.__enter__()
   while True:
-    data, addr = sock.recvfrom(1024)
-    # arr = np.frombuffer(data, dtype=np.dtype('d'))
+    data = conn.recv(1024)
     arr = np.frombuffer(data, dtype=np.float32)
     player.play(arr)
-    # output.play(arr, 44100)
+  conn.close()
 
 if __name__ == "__main__":
   play()
